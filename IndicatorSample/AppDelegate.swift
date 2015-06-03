bb189e70f7cf4c2e79e62cd3b6ce7a05a7954752
hayashidesu <katsu-h@w4.dion.ne.jp>
2015-06-03 10:45:29 +0900

    sample

--- IndicatorSample/AppDelegate.swift
+++ IndicatorSample/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  IndicatorSample
 //
-//  Created by 林克彦 on 2015/06/02.
+//  Created by Hayashidesu. on 2015/06/02.
 //  Copyright (c) 2015年 Hayashidesu. All rights reserved.
 //
 
--- IndicatorSample/ViewController.swift
+++ IndicatorSample/ViewController.swift
@@ -2,14 +2,21 @@
 //  ViewController.swift
 //  IndicatorSample
 //
-//  Created by 林克彦 on 2015/06/02.
+//  Created by Hayashidesu. on 2015/06/02.
 //  Copyright (c) 2015年 Hayashidesu. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func pressOn(sender: UIButton) {
+        SVProgressHUD.showWithStatus("loading...", maskType: SVProgressHUDMaskType.None)
+    }
 
+    @IBAction func pressOff(sender: UIButton) {
+        SVProgressHUD.dismiss()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
